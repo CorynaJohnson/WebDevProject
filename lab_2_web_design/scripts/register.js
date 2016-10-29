ecd4c8d777ad7f6eddda887290ee83c282c1590c
Coryna.Johnson <corynamarie@gmail.com>
2016-10-29 08:54:36 -0700

    finished lab4

--- lab_2_web_design/scripts/register.js
+++ lab_2_web_design/scripts/register.js
@@ -2,9 +2,21 @@
     var Fname = document.getElementById("FirstName");
     var Lname = document.getElementById("LastName");
     var Age = document.getElementById("Age");
-    var Year = document.getElementById("Year");
+    var Year;
+    if (document.getElementById('Year1').checked) {
+        Year = document.getElementById('Year1').value;
+    }
+    else if (document.getElementById('Year2').checked) {
+        Year = document.getElementById('Year2').value;
+    }
+    else if (document.getElementById('Year3').checked) {
+        Year = document.getElementById('Year3').value;
+    }
+    else if (document.getElementById('Year4').checked) {
+        Year = document.getElementById('Year4').value;
+    }
 
-    var result = Fname.toString() + Lname.toString() + Age.toString() + Year.toString();
+    var result = Fname.value + " " + Lname.value + " is " + Age.value + " years old and is a " + Year;
 
     var resultElement = document.getElementById("result");
 
